1d256bdaaa4d126da620d4c2c619c398d44d6d35
Carlos Ferreira <carlos@especializati.com.br>
2022-06-30 15:52:58 -0300

    First Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.multiply = void 0;
+function multiply(a, b) {
+    return a * b;
+}
+exports.multiply = multiply;
+console.log(multiply(2, 15));
---
+++ main.ts
@@ -0,0 +1,7 @@
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+console.log(multiply(2, 15));
+
+export { multiply }
